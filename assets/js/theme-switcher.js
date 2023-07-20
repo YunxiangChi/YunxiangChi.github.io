3480a65762429fe944916e4a7c3150084b4aa813
YunxiangChi <chi_yunxiang@163.com>
2023-07-20 14:30:41 -0500

    fix most UI issue with main page

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const switcher = document.getElementById('theme-switcher');
 
+    applyTheme();
+
     switcher.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
         var currentClass = document.body.className;
         if (currentClass == 'dark-mode') {
             switcher.innerHTML = '<i class="fa fa-moon"></i>';
+            // Store the theme in localStorage
+            localStorage.setItem('theme', 'dark');
         } else {
             switcher.innerHTML = '<i class="fa fa-sun"></i>';
+            // Store the theme in localStorage
+            localStorage.setItem('theme', 'light');
         }
     });
 });
+
+function applyTheme() {
+    var theme = localStorage.getItem('theme') || 'light';
+    if (theme === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.getElementById('theme-switcher').innerHTML = '<i class="fa fa-moon"></i>';
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.getElementById('theme-switcher').innerHTML = '<i class="fa fa-sun"></i>';
+    }
+}
